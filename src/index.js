8c3111402cd38f9a36180553a916adfcd7ec3272
janwee-sha <janwee_sha@outlook.com>
2026-07-20 12:08:20 +0800

    React 设计哲学。

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-import Game from "./official-docs/quick-start/square-chess-game";
+import Products from "./official-docs/quick-start/products";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<Game/>);
+root.render(<Products/>);
